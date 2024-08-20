43daa719e31642b8cb1e5d13981aff3321519741
pastisme <baptistecouland@gmail.com>
2024-08-20 00:24:37 +0000

    make platform=emscripten compile

--- scripts/genie.lua
+++ scripts/genie.lua
@@ -1179,9 +1179,6 @@ configuration { "asmjs" }
 		"-s USE_WEBGL2=1",
 		"-s LEGACY_GL_EMULATION=1",
 		"-s GL_UNSAFE_OPTS=0",
-		"--pre-js " .. _MAKE.esc(MAME_DIR) .. "src/osd/modules/sound/js_sound.js",
-		"--post-js " .. _MAKE.esc(MAME_DIR) .. "scripts/resources/emscripten/emscripten_post.js",
-		"--embed-file " .. _MAKE.esc(MAME_DIR) .. "artwork/slot-mask.png@artwork/slot-mask.png",
 	}
 	if _OPTIONS["SYMBOLS"]~=nil and _OPTIONS["SYMBOLS"]~="0" then
 		linkoptions {
